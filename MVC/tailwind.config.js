e9f4d5e7ae5d79cae0af54385d8189077b3e6c7d
rguerrer23 <ricardoguerrerocabrera@gmail.com>
2025-07-29 13:29:55 +0200

    landpage done, except config tailwind

---
+++ MVC/tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './src/templates/**/*.ejs',
+    './src/**/*.ts',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
