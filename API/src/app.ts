e5411d4f3891e603491ad92c7a8a50177c492724
juan-est145 <128648875+juan-est145@users.noreply.github.com>
2025-07-31 13:45:38 +0200

    The ssl certificates have been installed for all instances of the project

--- API/src/app.ts
+++ API/src/app.ts
@@ -1,12 +1,20 @@
 import { join } from 'node:path'
 import AutoLoad, { AutoloadPluginOptions } from '@fastify/autoload'
 import { FastifyPluginAsync, FastifyServerOptions } from 'fastify'
+import fs from 'node:fs'
 
 export interface AppOptions extends FastifyServerOptions, Partial<AutoloadPluginOptions> {
-
+  https: {
+    key: NonSharedBuffer;
+    cert: NonSharedBuffer;
+  }
 }
 // Pass --options via CLI arguments in command to enable these options.
 const options: AppOptions = {
+  https: {
+    key: fs.readFileSync("/etc/ssl/private/cert.key"),
+    cert: fs.readFileSync("/etc/ssl/certs/selfsigned.crt"),
+  }
 }
 
 const app: FastifyPluginAsync<AppOptions> = async (
