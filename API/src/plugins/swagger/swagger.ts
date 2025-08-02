7aa889aea6da8c78ecc0bf3693cb7c43f3518b82
juan-est145 <128648875+juan-est145@users.noreply.github.com>
2025-08-02 12:37:21 +0200

    Updated the swagger description

--- API/src/plugins/swagger/swagger.ts
+++ API/src/plugins/swagger/swagger.ts
@@ -7,8 +7,8 @@ export default fp<SwaggerOptions>(async (fastify) => {
 		openapi: {
 			openapi: "3.0.0",
 			info: {
-				title: "Swagger test",
-				description: "This is a test of fastify swagger with typebox",
+				title: "Transcendence API",
+				description: "This is a REST API to perform CRUD operations and account managament",
 				version: "1.0.0",
 			}
 		},
