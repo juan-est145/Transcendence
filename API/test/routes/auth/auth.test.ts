fc4b9c161612a51e9f21d1d3916dc44500d886f7
juan-est145 <128648875+juan-est145@users.noreply.github.com>
2025-08-06 21:53:42 +0200

    Tested most of the log-in endpoint

--- API/test/routes/auth/auth.test.ts
+++ API/test/routes/auth/auth.test.ts
@@ -1,20 +1,54 @@
 import { build } from "../../helper";
-import { test } from "node:test";
+import { it, describe } from "node:test";
 import { strictEqual } from "assert";
 
-test("Login route", async (t) => {
-	const app = await build(t);
+describe("Login tests", () => {
+	it("Incorrect credentials", async (t) => {
+		const app = await build(t);
 
-	const res = await app.inject({
-		url: "/v1/auth/log-in",
-		method: "POST",
-		headers: {
-			"content-type": "application/json"
-		},
-		body: {
-			email: "user@example.com",
-			password: "test"
-		}
+		const res = await app.inject({
+			url: "/v1/auth/log-in",
+			method: "POST",
+			headers: {
+				"content-type": "application/json"
+			},
+			body: {
+				email: "user@example.com",
+				password: "test"
+			}
+		});
+		strictEqual(res.statusCode, 401, "Returns a value of 401");
 	});
-	strictEqual(res.statusCode, 401, "Returns a value of 401");
+
+	it("Correct credentials", async(t) => {
+		const app = await build(t);
+
+		const res = await app.inject({
+			url: "/v1/auth/log-in",
+			method: "POST",
+			headers: {
+				"content-type": "application/json"
+			},
+			body: {
+				email: "user@example.com",
+				password: process.env.TEST_USER_PASSWD!
+			}
+		});
+		strictEqual(res.statusCode, 201, "Returns a value of 201");
+	});
+
+	it("Invalid fields", async (t) => {
+		const app = await build(t);
+
+		const res = await app.inject({
+			url: "/v1/auth/log-in",
+			method: "POST",
+			headers: {
+				"content-type": "application/json"
+			},
+			body: {}
+		});
+		strictEqual(res.statusCode, 400, "Returns a value of 400");
+	});
 });
+
