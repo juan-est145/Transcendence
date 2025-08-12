c87904324179d8b934d6374f70e0c451a5bc488b
juan-est145 <128648875+juan-est145@users.noreply.github.com>
2025-08-12 19:25:47 +0200

    Created documentation for login endpoints in MVC

--- MVC/src/routes/auth/log-in/log-in.service.ts
+++ MVC/src/routes/auth/log-in/log-in.service.ts
@@ -2,10 +2,22 @@ import { FastifyInstance } from "fastify";
 import { logInBody } from "./log-in.dto";
 import { LogInBody } from "./log-in.type";
 
+/**
+ * This function validates that the request body conforms to the zod object logInBody. If
+ * the request body is not valid, it throws a zod error.
+ * @param body - A fastify request body to be evaluated.
+ */
 export function validateLogInBody(body: unknown) {
 	logInBody.parse(body);
 }
 
+/**
+ * This function sends the user's credential to the REST API for loggin in. If the response
+ * code is between 400 or 500 it throws an exception.
+ * @param fastify - The fastify instance. It is decorated with the API client.
+ * @param body - A fastify request body with a username and password.
+ * @returns If successful it returns a JWT with some user data.
+ */
 export async function postLogin(fastify: FastifyInstance, body: LogInBody) {
 	const { email, password } = body;
 
--- MVC/src/routes/auth/log-in/log-in.ts
+++ MVC/src/routes/auth/log-in/log-in.ts
@@ -3,11 +3,33 @@ import { LogInBody, LogInError } from "./log-in.type";
 import { postLogin, validateLogInBody } from "./log-in.service";
 import { ZodError } from "zod";
 
+/**
+ * This module deals with everything relating to the login page.
+ */
 export async function auth(fastify: FastifyInstance) {
+	// TO DO: When sessions have been implemented, we must redirect the user if it is
+	// already logged in in all of these routes. We probably will need a hook for that.
+	
+	/**
+	 * This route sends to the client the login page. In case the user is already logged in,
+	 * it redirects him.
+	 * @param req - The fastify request instance.
+	 * @param res - The fastify response instance.
+	 * @returns The login page.
+	 */
 	fastify.get("/login", async (req, res) => {
 		return res.viewAsync("/log-in.ejs");
 	});
 
+	/**
+	 * This route logs the client if it's credentials are valid. If they aren't it sends
+	 * back the login page with 
+	 * it redirects him.
+	 * @param req - The fastify request instance. It must have a body property according to the
+	 * zod logInBody schema.
+	 * @param res - The fastify response instance.
+	 * @returns (Not yet implemented)
+	 */
 	fastify.post<{ Body: LogInBody }>("/login", async (req, res) => {
 		try {
 			validateLogInBody(req.body);
@@ -17,11 +39,11 @@ export async function auth(fastify: FastifyInstance) {
 
 		} catch (error) {
 			if (error instanceof ZodError) {
-				const ejsVariables = { errors: error.issues.map((element) => element.message) }
+				const ejsVariables = { errors: error.issues.map((element) => element.message) };
 				return res.status(400).viewAsync("/log-in.ejs", ejsVariables);
 			} else {
 				const logInError = error as LogInError;
-				const ejsVariables = { errors: logInError.details?.map((element) => element.msg) }
+				const ejsVariables = { errors: logInError.details?.map((element) => element.msg) };
 				return res.status(logInError.statusCode).viewAsync("/log-in.ejs", ejsVariables);
 			}
 		}
