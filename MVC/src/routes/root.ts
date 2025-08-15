8573c24bd754e9f5a87f1e0a2a6706c5e41eda0e
juan-est145 <128648875+juan-est145@users.noreply.github.com>
2025-08-15 20:10:54 +0200

    Request now has user property and UI updates when the user is logged in

--- MVC/src/routes/root.ts
+++ MVC/src/routes/root.ts
@@ -1,10 +1,18 @@
 import { FastifyPluginAsync } from 'fastify'
 import { auth } from './auth/log-in/log-in';
+import { DecodePayloadType } from '@fastify/jwt';
 
 const root: FastifyPluginAsync = async (fastify, opts): Promise<void> => {
+  fastify.addHook("onRequest", async (req, res) => {
+		let user: DecodePayloadType | null = null;
+		if (req.session.jwt) {
+			user = fastify.jwt.decode(req.session.jwt);
+		}
+    (req as any).user = user ? user : null;
+	});
+  
   fastify.get('/', async function (request, reply) {
-    const user = null;
-    return reply.viewAsync("index.ejs", { user });
+    return reply.viewAsync("index.ejs", { user: request.user });
   });
 
   fastify.register(auth, { prefix: "/auth" });
