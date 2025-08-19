e1eb454c9f043da750fe7dee84a04eb6932784b0
juan-est145 <128648875+juan-est145@users.noreply.github.com>
2025-08-19 17:14:42 +0200

    Added a basic options object to helmet in MVC

--- MVC/src/plugins/helmet/helmet.ts
+++ MVC/src/plugins/helmet/helmet.ts
@@ -2,5 +2,9 @@ import helmet, { FastifyHelmetOptions } from "@fastify/helmet";
 import fp from "fastify-plugin";
 
 export default fp<FastifyHelmetOptions>(async(fastify) => {
-	fastify.register(helmet);
+	const opts: FastifyHelmetOptions = {
+		global: true,
+	};
+	fastify.register(helmet, opts);
 });
+//https://images.pexels.com/photos/31504039/pexels-photo-31504039.jpeg
