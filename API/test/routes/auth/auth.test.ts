a59da11c43df7fabf1d10e8662f0362b5a9f617f
juan-est145 <128648875+juan-est145@users.noreply.github.com>
2025-08-28 11:20:55 +0200

    All of the tests in API have been rewritten

--- API/test/routes/auth/auth.test.ts
+++ API/test/routes/auth/auth.test.ts
@@ -4,7 +4,7 @@ const app = build();
 
 describe("Login tests", () => {
 	it("Incorrect credentials", async () => {
-		const errorSpy = jest.spyOn(console, 'error').mockImplementation(() => {});
+		const errorSpy = jest.spyOn(console, 'error').mockImplementation(() => { });
 		const res = await app.inject({
 			url: "/v1/auth/log-in",
 			method: "POST",
@@ -18,61 +18,35 @@ describe("Login tests", () => {
 		});
 		expect(res.statusCode).toEqual(401);
 		errorSpy.mockRestore();
-	})
+	});
+
+	it("Correct credentials", async () => {
+		const res = await app.inject({
+			url: "/v1/auth/log-in",
+			method: "POST",
+			headers: {
+				"content-type": "application/json"
+			},
+			body: {
+				email: "user@example.com",
+				password: process.env.TEST_USER_PASSWD!
+			}
+		});
+		expect(res.statusCode).toEqual(201);
+	});
+
+	it("Invalid fields", async () => {
+		const errorSpy = jest.spyOn(console, 'error').mockImplementation(() => { });
+		const res = await app.inject({
+			url: "/v1/auth/log-in",
+			method: "POST",
+			headers: {
+				"content-type": "application/json"
+			},
+			body: {}
+		});
+		expect(res.statusCode).toEqual(400);
+		errorSpy.mockRestore();
+	});
 });
-
-
-// import { build } from "../../helper";
-// import { it, describe } from "node:test";
-// import { strictEqual } from "assert";
-
-// describe("Login tests", () => {
-// 	it("Incorrect credentials", async (t) => {
-// 		const app = await build(t);
-
-// 		const res = await app.inject({
-// 			url: "/v1/auth/log-in",
-// 			method: "POST",
-// 			headers: {
-// 				"content-type": "application/json"
-// 			},
-// 			body: {
-// 				email: "user@example.com",
-// 				password: "test"
-// 			}
-// 		});
-// 		strictEqual(res.statusCode, 401, "Returns a value of 401");
-// 	});
-
-// 	it("Correct credentials", async(t) => {
-// 		const app = await build(t);
-
-// 		const res = await app.inject({
-// 			url: "/v1/auth/log-in",
-// 			method: "POST",
-// 			headers: {
-// 				"content-type": "application/json"
-// 			},
-// 			body: {
-// 				email: "user@example.com",
-// 				password: process.env.TEST_USER_PASSWD!
-// 			}
-// 		});
-// 		strictEqual(res.statusCode, 201, "Returns a value of 201");
-// 	});
-
-// 	it("Invalid fields", async (t) => {
-// 		const app = await build(t);
-
-// 		const res = await app.inject({
-// 			url: "/v1/auth/log-in",
-// 			method: "POST",
-// 			headers: {
-// 				"content-type": "application/json"
-// 			},
-// 			body: {}
-// 		});
-// 		strictEqual(res.statusCode, 400, "Returns a value of 400");
-// 	});
-// });
 
