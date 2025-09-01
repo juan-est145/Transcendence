e94d164815319f51c4a3dc3be0a13a2ea4bf9264
juan-est145 <128648875+juan-est145@users.noreply.github.com>
2025-09-01 12:21:01 +0200

    Managed to add a new user to minio and store a file programatically

---
+++ minio/entrypoint.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+set -e
+
+# Start MinIO in the background
+minio server /data --console-address ":9001" -S /etc/ssl/certs &
+MINIO_PID=$!
+
+# Wait for MinIO to be ready
+until curl -Isfk https://localhost:9000/minio/health/live 1>/dev/null; do
+  sleep 1
+done
+
+# Add user
+mc alias set local https://localhost:9000 "${MINIO_ROOT_USER}" "${MINIO_ROOT_PASSWORD}" --insecure
+mc admin policy attach local readwrite --user "${MINIO_NODE_USER}"
+mc admin user add local "${MINIO_NODE_USER}" "${MINIO_NODE_PASSWORD}"
+
+# Bring MinIO to foreground
+wait $MINIO_PID
