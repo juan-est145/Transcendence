c52047f26a49d143240bc61d8932b1808e34d293
Jorge Callejo Martinez <jcallejo@c2r6s6.42malaga.com>
2025-09-01 14:50:26 +0200

    I'm coiling the logic of colision too much, will have to change it to be more like 2d collision at least for now, but mostly done, not really functional yet

--- MVC/src/routes/renamePong/3d.ts
+++ MVC/src/routes/renamePong/3d.ts
@@ -3,7 +3,7 @@ import { FastifyPluginAsync } from 'fastify';
 console.log("pong3dRoutes loaded");
 
 /**
- * Represents the current state of the game and each numeric value of every element in 2d pong
+ * Represents the current state of the game and each numeric value of every element in 3d pong
  */
 const gameState = {
     scoreOne: 0,
@@ -25,7 +25,7 @@ function movePaddles(input?: { paddleOne?: number, paddleTwo?: number, paddleOne
     gameState.paddleTwo.velocityY = input?.paddleTwoVelocity ?? 0;
 }
 
-function isColliding(ball, paddle) {
+function isColliding(paddle, ball) {
 	const paddleMin = {
 		x: paddle.x - paddle.width / 2,
 		y: paddle.y - paddle.height / 2,
@@ -50,3 +50,78 @@ function isColliding(ball, paddle) {
 		ballMin.y <= paddleMax.y
 	);
 }
+
+function ballWallCollision(){
+	if (gameState.ball.y < 0.2 || gameState.ball.y > 2.8) {
+		gameState.ball.velocityY *= -1;
+	}
+
+	[gameState.paddleOne, gameState.paddleTwo].forEach(paddle => {
+		if (isColliding(gameState.ball, paddle)){
+			const hitPos = (gameState.ball.y - paddle.y) + paddle.height / 2;
+			const relativeIntersect = hitPos / paddle.height;
+			const direction = (relativeIntersect < 0.5 ? -1 : 1);
+			gameState.ball.velocityY = direction * Math.abs(gameState.ball.velocityY);
+			if (idx === 0){
+				gameState.ball.velocityX = Math.abs(gameState.ball.velocityX);
+				gameState.ball.x = paddle.x + paddle.width / 2 + gameState.ball.radius;
+			} else {
+				gameState.ball.velocityX = -Math.abs(gameState.ball.velocityX);
+				gameState.ball.x = paddle.x - paddle.width / 2 - gameState.ball.radius;
+			}
+		}
+	});
+
+	if (gameState.ball.x < -4.8) {
+		gameState.scoreTwo++;
+		resetBall();
+	} else if (gameState.ball.x > 4.8) {
+		gameState.scoreOne++;
+		resetBall();
+	}
+}
+
+
+function resetBall() {
+	gameState.ball.x = 0;
+	gameState.ball.y = 1;
+	gameState.ball.z = 0;
+	gameState.ball.velocityX = 0.05;
+	gameState.ball.velocityY = 0.05 * (Math.random() > 0.5 ? 1 : -1);
+	gameState.ball.velocityZ = 0;
+}
+
+function tickGame(input?: { paddleOne?: number, paddleTwo?: number, paddleOneVelocity?: number, paddleTwoVelocity?: number }) {
+	movePaddles(input);
+	gameState.ball.x += gameState.ball.velocityX;
+	gameState.ball.y += gameState.ball.velocityY;
+	ballWallCollision();
+}
+
+const pong3dRoutes: FastifyPluginAsync = async (fastify, options) => {
+
+	fastify.get('/state', async (request, reply) => {
+		return gameState;
+	});
+
+	fastify.post("/move", async (request, reply) => {
+		movePaddles(request.body as any);
+		return gameState;
+	});
+
+	fastify.post('/tick', async (request, reply) => {
+		tickGame(request.body as any);
+		return gameState;
+	});
+
+	fastify.post('/reset', async (request, reply) => {
+		gameState.scoreOne = 0;
+		gameState.scoreTwo = 0;
+		gameState.paddleOne.y = 1;
+		gameState.paddleTwo.y = 1;
+		resetBall();
+		return gameState;
+	});
+};
+
+export default pong3dRoutes;
